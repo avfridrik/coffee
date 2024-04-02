c96c9b543aa70df7b4ed5456b4cb2614f1c50649
Alexandr <7989288+avfridrik@users.noreply.github.com>
2024-04-02 20:21:50 +0300

    fix production mode

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,6 +2,8 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NODE_ENV === "production" ? undefined : "http://localhost:3001";
+  process.env.NODE_ENV === "production"
+    ? "http://localhost:3001"
+    : "http://localhost:3001";
 
 export const socket = io(URL);
